2d7d1e6e163988183f6d0284053f3a06f3db3ec0
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-08 15:05:56 +0200

    write users#new,create,index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to users_path
+  end
+
+  def index
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
