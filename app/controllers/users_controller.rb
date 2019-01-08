3bf0872b304d4a40cfa87a0f977f7e716ed73c07
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-08 15:11:01 +0200

    handle pw validation, pass user-controller spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    session[:user_id] = user.id
-    redirect_to users_path
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to users_path
+    else 
+      redirect_to new_user_path
+    end
   end
 
   def index
