43d0cda48c122380d3e07d7ecb5ee636c130f688
Cheng Vue <csalvue@gmail.com>
2019-02-03 14:08:21 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    if params[:user][:password] == params[:user][:password_confirmation]
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
