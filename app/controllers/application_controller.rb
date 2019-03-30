43fe170ef67c45271e83ac3a35d7f2ef0ac41f87
Cookie McCormick <muntananuchat@gmail.com>
2019-03-29 22:19:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  helper_method :current_user
+
   private
 
   def require_login
