9109fd2845ac3d73648027314f42bb62717d5352
Rafael Batista <RafaelB24@yahoo.com>
2019-04-26 18:54:40 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(:name => params[:user][:name])
+    session[:user_id] = @user.id if @user.authenticate(params[:user][:password])
+  end
+end
