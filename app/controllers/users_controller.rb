74f5716f31f21329cacedb9681eae8512ba5aa37
Diana Badas <diana.badas@gmail.com>
2019-06-11 22:38:06 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to controller: 'wellcome', action: 'homepage'
     else
-      return redirect_to controller: 'users', action: 'new'
+      redirect_to controller: 'users', action: 'new'
     end
   end
 
