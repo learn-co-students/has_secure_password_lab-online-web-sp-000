7e3fd3ec789e7967667fe6b531d9d95cedc1c640
github username <github email address>
2019-06-20 17:34:14 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/"
+    else
+      render "user/new"
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
