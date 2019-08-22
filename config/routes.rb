f14688ee038e7dbe890831fbddeda1296e5e6e8c
Bry Cox <carl.cc@gmail.com>
2019-08-22 17:34:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  post '/login' => 'sessions#create'
+  get '/welcome' => 'welcome#new'
+  resources :users, only: [:create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
