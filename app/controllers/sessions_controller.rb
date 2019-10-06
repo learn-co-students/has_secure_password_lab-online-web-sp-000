2f7096a3c0a567892c6f5241cf76c83127cec0b9
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-06 11:37:30 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(name: params[:name])
-        return head(:forbidden) unless @user.authenticate(parms[:password])
-        session[:user_id] = @user.id
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_url
+        else
+            redirect_to '/new'
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/users/new' to: 'users#new'
-post '/users/new' to: 'users#create'
+resources :users
+resources :sessions, only: [:new, :create]
+
+get '/login', to: 'sessions#new'
+# get '/users/new', to: 'users#new'
+# post '/users/new', to: 'users#create'
 
 end
