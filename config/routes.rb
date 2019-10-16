845ca345e37fff780f36355bb6a9a0007ea368e3
Brian Simonton <bsimonton78@gmail.com>
2019-10-16 04:26:12 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
+  resources :users
+  get '/login' => 'sessions#new'
+  
+   resources :sessions
+  post '/login' => 'session#create'
+  post '/logout' => 'session#destroy'
+  
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/001_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+ 
+  def up
+    create_table :users do |t|
+      t.string :name
+      
+      t.string :password_digest
+    end
+  end
+
+  def down
+    drop_table :users
+    
+  end
+end
