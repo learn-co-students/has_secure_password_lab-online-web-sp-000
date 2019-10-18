17bb5a2c947e371719cad01b1f5408939c7340eb
Becky Phan <beckyphan@live.com>
2019-10-17 23:48:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
+  get '/home', to: 'sessions#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Prefix Verb URI Pattern          Controller#Action
+#    users POST /users(.:format)     users#create
+# new_user GET  /users/new(.:format) users#new
+#    login GET  /login(.:format)     sessions#new
+#   logout POST /logout(.:format)    sessions#destroy
