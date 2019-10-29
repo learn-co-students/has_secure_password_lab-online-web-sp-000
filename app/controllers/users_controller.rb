fbbe2d0097c137752d32e8b9736159139a703d4e
jgrantps <jgrantps@hotmail.com>
2019-10-29 16:04:28 -0300

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      redirect_to '/users/new'
+    end
+
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
