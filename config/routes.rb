3dbea485ef7bde159d9c55830a0087b9f2234f42
boom100100 <bsd231@nyu.edu>
2019-12-23 17:37:50 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new' #doesn't require view
+  post '/login', to: 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/users/new', to: 'users#new'
+  post '/users' => 'users#create'
+  root to: 'welcome#home'
+
 end
