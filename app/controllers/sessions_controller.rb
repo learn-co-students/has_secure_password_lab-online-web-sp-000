56e58870209ddd6768214ee2c2233e2112484f71
Christian Perez <perezchristian1012@gmail.com>
2020-01-20 19:17:00 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      flash[:notice] = "Login is incorrect"
+      redirect_to :login
+    end
+  end
+end
