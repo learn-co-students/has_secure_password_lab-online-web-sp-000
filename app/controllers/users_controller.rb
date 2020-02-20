62ab75f9e4811e0d91b19423a4d274f94e129cda
TrueC-D <aletheacary@gmail.com>
2020-02-19 20:47:30 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if params[:password] == params[:password_confirmation]
-      @user.save
+      if @user.save
       session[:user_id] = @user.id
-      # need to make this dry - how to run controller: 'session' action: 'create' properly?
+      # need to make this dry - how to run controller: 'session' action: 'create' properly? sessions are not the Users controller's concern.
+      # a helper method in for the sessions controller would work. Has both the Users controller and the Sessions controller user the login method
+      else 
+        redirect_to controller: 'users', action: 'new' 
+      end
     else
-      redirect_to(controller: 'users', action: 'new')
+      redirect_to controller: 'users', action: 'new'
       # render :new
     end
+  
   end
 
   private
