95bb3c20a089d14b0137286971921a6ce0be4824
rohanphillips <rohan@solutions-insurance.com>
2020-02-21 08:13:01 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/show'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  
   resources :users
+  resources :welcome, only: [:show]
+  resources :sessions, only:[:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200221123543_create_users.rb
+++ db/migrate/20200221123543_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :first_name
+      t.string :name
+      t.string :password
       t.string :password_digest
 
       t.timestamps
