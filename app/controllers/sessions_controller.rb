89ded4c7826c89f19481df727d8a96063bf6b80e
mlgvla <mlgerardvla@gmail.com>
2020-06-30 21:56:49 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+   
+    def create
+        user = User.find_by(name: params[:user][:name])
+        @user = user.try(:authenticate, params[:user][:password])
+        return head(:forbidden) unless @user
+        session[:user_id] = @user.id
+        
+        redirect_to '/users/show'
+    end
+end
