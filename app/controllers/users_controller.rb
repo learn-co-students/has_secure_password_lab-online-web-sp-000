b5f42bc1b4a607a75ca4aee1fee4504bf876c198
CoderJay06 <jaycru06@yahoo.com>
2020-07-11 01:46:04 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
     # raise params.inspect
     @user = User.create(user_params)
+    redirect_to '/users/new' unless password_match_confirmation
     session[:user_id] = @user.id 
   end
 
@@ -10,4 +11,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
+
+  def password_match_confirmation
+    params[:user][:password] == params[:user][:password_confirmation]
+  end 
 end
