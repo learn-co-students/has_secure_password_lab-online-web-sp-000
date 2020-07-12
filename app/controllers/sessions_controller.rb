0941b2482ac7ecba43412e88b53274e3ea8bdd21
CoderJay06 <jaycru06@yahoo.com>
2020-07-12 01:08:45 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    # byebug
-    # user = User.find_by(name: params[:name])
-    # authenticated = user.try(:authenticate, params[:password])
-    # return head(:forbidden) unless authenticated
-    # @user = user  
-    # session[:user_id] = @user.id 
-    # byebug
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.try(:authenticate, params[:user][:password])
     session[:user_id] = @user.id 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
 
   get 'users/create'
 
+  get 'welcome/home'
+
   post 'sessions/create'
+
+  post '/welcome/home'
+
+  post '/users/new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
