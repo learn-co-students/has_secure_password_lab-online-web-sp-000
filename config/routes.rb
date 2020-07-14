58fda3e583c4abf851d20ed55ffdda27a3771488
carrollm2 <carrollm2@gmail.com>
2020-07-13 23:41:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users', to: 'users#new'
+  post '/users', to: 'users#create'
+  
+  get '/', to: 'sessions#new'
+  post '/', to: 'sessions#create'
+
+  post '/logout', to: 'sessions#destroy'
 end
