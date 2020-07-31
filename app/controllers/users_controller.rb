c75be0b1e2dbbec6e94873050463f5673fea72e0
ksummerill <kaitlynsummerill@gmail.com>
2020-07-31 07:58:33 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def create
+    # if params[:name]
+     User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
