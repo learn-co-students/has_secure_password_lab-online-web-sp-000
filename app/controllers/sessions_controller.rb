335199eb1eebc2b8dfb3d995f7800767bd01c975
micahjwhite <micahjwhite@gmail.com>
2020-08-01 15:47:08 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(name: user_params[:name])
+    return head(:forbidden) unless @user.authenticate(user_params[:password])
+    session[:user_id] = @user.id
+  end
+
+end
---
+++ app/helpers/u_sessions_helper.rb
@@ -0,0 +1,2 @@
+module USessionsHelper
+end
