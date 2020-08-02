af6fd6518a09dc90db1b5724cc6c348d07af432c
rem <chrisxcheong@gmail.com>
2020-08-01 22:15:43 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
+  root 'welcome#home'
 
-  get 'users/new'
+  resources :users, only: [:new, :create]
 
-  get 'users/create'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
