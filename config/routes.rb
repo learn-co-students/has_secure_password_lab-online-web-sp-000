d285cf42d358acb2a27a5263599f6effbe95f1e5
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-07 01:38:58 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  root 'users#index'
+
 end
