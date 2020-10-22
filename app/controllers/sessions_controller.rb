a1a6f52015b6a86527ae75634651c279320ebf2f
yarvinh <yarvinhhernandez@gmail.com>
2020-10-21 21:41:57 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController 
+    def new
+        @user = User.new
+    end
+    def create
+        p params[:user]
+        @user = User.find_by(name: params[:user][:name])
+        return head(:forbidden) unless @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+      end
+end
