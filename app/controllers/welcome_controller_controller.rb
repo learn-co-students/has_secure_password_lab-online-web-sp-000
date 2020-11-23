a6d5ed57219fe47bc8a42222b2b168b27dcb6e1d
seth <seth.j.near@gmail.com>
2020-11-23 09:55:26 -0600

    Done.

---
+++ app/controllers/welcome_controller_controller.rb
@@ -0,0 +1,4 @@
+class WelcomeControllerController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root 'application#hello'
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/users/homepage' => 'welcome_controller#show'
+  post '/logout' => 'sessions#destroy'
+
+
 end
