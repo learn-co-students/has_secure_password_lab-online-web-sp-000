75acba43ddbc55cf71615c687c18559b2b094bbf
Drew Thompson <drewallynthompson@gmail.com>
2020-12-08 08:40:41 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new 
+  end
+  
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      redirect_to :login, alert: "Login is Incorrect"
+    end
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root :to => 'sessions#login'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/welcome' => 'users#welcome'
 end
