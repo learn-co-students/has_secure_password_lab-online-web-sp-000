f15acf133bcf2ac5103e351517bc4cc2cd49bbc9
jrog20 <jaimerogstad@gmail.com>
2020-12-17 18:46:42 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if params[:password].blank?
+      redirect_to '/login'
+    else 
+      session[:user_id] = user.id
+      redirect_to '/'
+    end
+  end
 end
