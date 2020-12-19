e4830f97ecbe97051468eabc49276226dc097589
jrog20 <jaimerogstad@gmail.com>
2020-12-18 19:05:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # get 'user/new'
   # get 'user/create'
-  # resources :users
-  # resources :sessions
+  resources :users
+  resources :sessions
 
-  get '/' => 'users#homepage'
-  resources :users, :only => [:new, :create]
+  # get '/' => 'users#homepage'
+  # resources :users, :only => [:new, :create]
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
 end
