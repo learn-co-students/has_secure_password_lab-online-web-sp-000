ea04a354327d0ddf0b6b9a241ff0e3d8ce41502f
Arodriguez1030 <arodriguez1030@gmail.com>
2021-01-23 09:53:18 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            redirect_to login_path
+        end
+    end
+end
