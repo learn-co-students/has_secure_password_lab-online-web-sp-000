a4e66a055efcd73e95183d88d6d27aacdbdd9848
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-21 08:53:07 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
     end
  
     def create 
+        @user = User.find_by(name: params[:user][:name])
         if @user.name && @user.authenticate(params[:user][:password])
-            @session = session
-            @session[:user_id] = @user.params[:user][:id]
+            session[:user_id] = @user[:id]
             redirect_to welcome_path(@user)
         else
             render :'users/new'
@@ -22,13 +22,13 @@ class SessionsController < ApplicationController
     
 private 
     
-    def authenticate (password)
-        @user = User.find_by_name(params[:name])
-        if @user.password && @user.password == params[:password]
-            return true
-        else 
-            return nil 
-        end
-    end 
+    # def authenticate (password)
+    #     @user = User.find_by_name(params[:name])
+    #     if @user.password && @user.password == params[:password]
+    #         return true
+    #     else 
+    #         return nil 
+    #     end
+    # end 
 
   end
