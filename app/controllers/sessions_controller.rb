9c5581c0b5f4600e3e13becf7e896b8c07752ab5
deliaconstantino <dconstan92@gmail.com>
2021-03-05 13:49:44 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,33 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # raise params.inspect
-    # @user = User.find_by(name: params[:name])
-    # redirect_to '/login' unless @user.authenticate(params[:password])
-    # session[:user_id] = @user.id
-
-
-    @user = User.find_by(name: params[:name])
-    if @user
-      if @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to '/welcome'
-      else
-        redirect_to '/login'
-      end
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
     else
       redirect_to '/login'
     end
-
-    # User.find_by(name: 'david')&.authenticate('notright')
-    # @user = User.find_by(name: params[:name])&.authenticate(params[:password])
-
-    # if @user
-    #   # session[:user_id]).to eq(connie.id)
-    #   session[:user_id] = @user.id
-    #   redirect_to '/welcome'
-    # else
-    #   redirect_to '/login'
-    # end
   end
+
 end
