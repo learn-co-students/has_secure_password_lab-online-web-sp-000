2fcdf4e2bf2d1852b60113b806c1550f405b024f
Jeff Bai <j.bai@wustl.edu>
2021-03-18 07:40:00 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get '/login', to: "sessions#login"
+  post '/login', to: "sessions#create"
+  get '/welcome', to: "welcome#homepage"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
