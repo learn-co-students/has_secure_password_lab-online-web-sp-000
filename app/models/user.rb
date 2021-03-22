dcf2cc77d85b700999aa9ec866274aa7aab4e2bf
joseph <josephtkao@gmail.com>
2021-03-22 17:05:24 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates :password, presence: true
+    validates :password, confirmation: true
+
+  
+
+    def authenticate(string)
+        if self.password == string
+            return self
+        end
+    end
 end
