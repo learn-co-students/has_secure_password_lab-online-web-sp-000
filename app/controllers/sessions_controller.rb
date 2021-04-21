a8a376a086c38dc64fb8fed302faa8861c086a36
amongiove <amandamongiove@gmail.com>
2021-04-21 11:55:51 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,16 @@
-require 'pry'
 class SessionsController < ApplicationController
 
   def new
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    #binding.pry
-    if @user.present? && @user.authenticate(params[:user][:password])
-          
-
-      session[:user_id] = @user.id
-      redirect_to root_path(@user)
-    else
-      redirect_to controller: 'sessions', action: 'new'
-    end
+    user = User.find_by(name: params[:user][:name])
+    @user = user.try(:authenticate, params[:user][:password])
+    return redirect_to controller: 'sessions', action: 'new' unless @user
+    session[:user_id] = user.id
+    redirect_to root_path(@user)
+    
   end
-
 
   def destroy
     session.delete :user_id
