5c59c6a1d1efcba7bc5680a73ff765238cabb76d
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-06-01 16:36:22 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,17 @@ class UsersController < ApplicationController
     def create # if password and password confirmation don't match
             # redirect_to signup_path unless params[:password] == params[:password_confirmation]
             #     @user = User.new(user_params)
-            if params[:password] != params[:password_confirmation]
-                redirect_to controller: 'users', action: 'new'
-            else
-                @user = User.new(user_params)
+            @user = User.new(user_params)
                 if @user.save
                     session[:user_id] = @user.id
                     redirect_to root_path
+                else
+                    redirect_to controller: 'users', action: 'create'
                 end
-            end
     end
 
     def welcome
+        # byebug
         @user = User.find_by(params[:user_id])
         render :welcome
     end
