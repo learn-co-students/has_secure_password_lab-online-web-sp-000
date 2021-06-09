ba174950b4d02798ecd862132d84d206daede0e1
JustinBreneman <Justin.Breneman@yahoo.com>
2021-06-09 14:59:14 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/signup', to: 'users#new'
+  post 'users/create'
+
+  get 'users/login'
+  
+  post 'sessions/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
