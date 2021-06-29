0980512888b65c0e322e6057aaa6b8db43a357e3
William Pannell <lpann5@gmail.com>
2021-06-28 23:07:57 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,10 @@ class SessionsController <  ApplicationController
     end
     def create
         user = User.find_or_create_by(name: params[:user][:name])
-        user = user.try(:authenticate, params[:user][:name])
-        return redirect_to(controller: 'sessions', action: 'new') unless user
+        user = user.try(:authenticate, params[:user][:password])
+        if !user
+            return redirect_to(controller: 'sessions', action: 'new')
+        end
         session[:user_id] = user.id
         @user = user
         redirect_to controller: 'users', action: 'home'
@@ -12,6 +14,6 @@ class SessionsController <  ApplicationController
     def destroy
         session.delete :user_id
         redirect_to '/'
-      end
+    end
   end
   
